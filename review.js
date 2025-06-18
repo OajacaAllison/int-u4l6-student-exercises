372b4340981cc986d665e700a68e42c531ef44b1
OajacaAllison <oajacaallison@gmail.com>
2025-06-18 09:33:40 -0400

    meow

--- review.js
+++ review.js
@@ -13,13 +13,22 @@ testButton.addEventListener("click", function() {
 
     // 1. If the number is less than 30, a message will be displayed on the web page.
     //  - The message should read: "The number is less than 30."
-    
+    if (randomNumber < 30) {
+        result.innerHTML = "The Number is less than 30." 
+        result.style.color= "red";
+    } 
     // 2. If the number is between 30 and 69, a message will be logged to the console.
     //  - The message should read: "The number is between 30 and 69."
-    
+    else if (randomNumber >= 30 && randomNumber <= 69) {
+        result.innerHTML = "The number is between 30 and 69. ";
+        result.style.color= "green";
+    }
     // 3. If the number is 70 or greater, a different message will be displayed on the web page.
     //  - The message should read: "The number is 70 or greater."
- 
+    else {
+        result.innerHTML = "The number is 70 or greater. " + randomNumber;
+        result.style.color = "red";
+    }
     // Extensions: 
     //  - Try modifying the conditions or the actions taken (such as changing the text or adding new conditions).
     //  - Try adding an input and test conditions for the random number and user input.
